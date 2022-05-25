3a62c397ac3b8bab0d733078ec20fb8c93143f91
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-05-25 23:15:32 +0530

    Time: 120 ms (56.07%), Space: 53.4 MB (21.62%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       int n=nums.size();
-       unordered_set<int> uset; //bcz set property does't allow duplicate
-        
-       for(int i=0;i<n;i++)
-       {
-           uset.insert(nums[i]);
-       }
-        
-       if(n!=uset.size()) return true;
-        
-    
-       return false;
-        
-        
+        unordered_map<int,int>m;
+        for(int i = 0; i < nums.size(); i++) 
+        {
+            m[nums[i]]++;
+        }
+        for(auto i : m) if(i.second >= 2) return true;
+        return false;
     }
 };
+
+
+
+
+// unordered_set<int> uset; //bcz set property does't allow duplicate  
+//        for(int i=0;i<n;i++)
+//        {
+//            uset.insert(nums[i]);
+//        }
+//        if(n!=uset.size()) return true;
+//        return false;
