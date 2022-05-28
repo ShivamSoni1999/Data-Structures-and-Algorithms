e15b2d83ae0da3cf76aa3e18091942aa9760d3a3
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-05-29 03:37:27 +0530

    Time: 12 ms (83.91%), Space: 10.9 MB (29.68%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> index;
-        int size=nums.size();
-        for(int i=0;i<size;i++)
+        
+        int n=nums.size();     // O(N),O(N) sol
+        vector<int> ans;
+        unordered_map<int, int> umap;
+
+        for(int i=0;i<=n;i++)
         {
-            int k=target-nums[i];
-            for(int j=i+1;j<size;j++)
+            int rem_value=target-nums[i];
+            if(umap.find(rem_value)!=umap.end()) //if this is true then I got the value in hashmap
             {
-                if(nums[j]==k)
-                {
-                    index.push_back(i);
-                    index.push_back(j);
-                    break;
-                }
+                ans.push_back(umap[rem_value]); //pushing value in ans vector
+                ans.push_back(i);               //pushing index too
+                return ans;
             }
-            if(index.size()==2)
-                break;
+            else
+                umap[nums[i]] = i;              //storing value and index if no value found in hashmap
+                
         }
-        return index;
+        
+        return ans;
+    
     }
-	//Time Complexity: O(n^2)
-    
-    
 };
