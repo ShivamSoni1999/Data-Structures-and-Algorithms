ce9a4799ca459c1972afd913f3e038103e5e5320
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-05-29 17:51:23 +0530

    Time: 49 ms (19.52%), Space: 19.3 MB (14.45%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-      
-         stable_partition(begin(nums),end(nums),[](int i){return i;});
-        
+    void moveZeroes(vector<int>& nums) 
+    {
+        int left=0,right=0;
+        int n = nums.size();
+        while(left<n)
+        {
+            if(nums[left]!=0)   //looking for non zero element
+            {
+                swap(nums[left],nums[right]);
+                left++;
+                right++;
+            }
+            else
+                left++;
+        }
     }
 };
