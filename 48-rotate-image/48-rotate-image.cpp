9bdbaae056f542b55f2947ea4ae61fc0562912c6
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-05-30 18:24:01 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (34.04%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -9,10 +9,15 @@ public:
             for(int j=0;j<i;j++)
                 swap(matrix[i][j], matrix[j][i]);
         
-		//reversing evry row
-        for(int i=0;i<n;i++)
-        {
-            reverse(matrix[i].begin(),matrix[i].end());
+		for(int i=0; i<n; ++i) {
+		// 2 Pointer approach :  just like we do in 1D array we take left and right pointers
+		// and swap the values and then make those pointers intersect at some point.
+            int left = 0, right = n-1;
+            while(left < right) {
+                swap(matrix[i][left], matrix[i][right]);
+                ++left;
+                --right;
+            }
         }
         
        
