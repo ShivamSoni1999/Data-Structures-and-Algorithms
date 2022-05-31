c71bf349bb36675bee7ca7566704cc26d8bc3c8d
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-05-31 20:13:30 +0530

    Time: 0 ms (100.00%), Space: 7 MB (73.59%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,23 +3,23 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         
         int n=matrix.size();
-        
-		//transpose the matrix
-        for(int i=0;i<n;i++)
+        int degrr=1;
+        while(degrr--)
+        {
+            //transpose the matrix
+          for(int i=0;i<n;i++)
             for(int j=0;j<i;j++)
-                swap(matrix[i][j], matrix[j][i]);
+              swap(matrix[i][j], matrix[j][i]);
         
-		for(int i=0; i<n; ++i) {
-		// 2 Pointer approach :  just like we do in 1D array we take left and right pointers
-		// and swap the values and then make those pointers intersect at some point.
-            int left = 0, right = n-1;
-            while(left < right) {
-                swap(matrix[i][left], matrix[i][right]);
-                ++left;
-                --right;
-            }
+		//reversing evry row
+        for(int i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
         }
         
+            
+        }
+		
        
         
     }
