685912ca57ad4b32234e547de47d3fa7e7ef84ac
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-06-01 16:09:38 +0530

    Time: 27 ms (64.29%), Space: 23.3 MB (41.82%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n = s.size();
+        
         ios_base::sync_with_stdio(0);
         cin.tie(0); cout.tie(0);
         int left=0;
-        int right=n-1;
+        int right=s.size()-1;
         while(left<right)
         {
             swap(s[left],s[right]);
