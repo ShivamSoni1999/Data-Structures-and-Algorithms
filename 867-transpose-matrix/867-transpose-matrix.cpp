63fad4e87f4d239fb68452f86e3d018bed81c2f7
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-06-02 15:33:09 +0530

    Time: 12 ms (81.32%), Space: 10.6 MB (78.16%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) 
     {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
+        
     // Create a result 2D matrix to store the transpose
 	int m = matrix.size(), n = matrix[0].size();
 	vector<vector<int>> result(n, vector<int>(m));
