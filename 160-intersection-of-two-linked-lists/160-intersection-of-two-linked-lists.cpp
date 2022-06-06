dc4e023623e4122e96ffad8943cd95a447a23f84
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-06-06 21:42:46 +0530

    Time: 43 ms (94.98%), Space: 14.4 MB (88.39%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,27 @@
+//striver sol
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA==NULL || headB==NULL) return NULL;
+            
+        ListNode *a = headA;
+        ListNode *b = headB;
+        
+        while(a!=b) //keep on traversing until both of them are equal
+        {
+            if(a==NULL) 
+                a=headB;   //if a reaches null make it point to other lists head 
+            else
+            a=a->next;           //otherwise  keep on moving..
+            
+            if(b==NULL) 
+                b=headA;  
+            else
+                b=b->next;    
+        }
+        
+        return a;       //If a==b then the then it means both standing on intersection or null
+        
+       
+    }
+};
