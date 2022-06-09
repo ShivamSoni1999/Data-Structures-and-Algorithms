73842bed5dedf916cd75573f8dc1d0f4f7ba06d2
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-06-09 22:47:39 +0530

    Time: 3 ms (96.80%), Space: 9 MB (31.49%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,49 +1,35 @@
 class Solution {
   public:
-    vector < int > nextGreaterElement(vector < int > & nums1, vector < int > & nums2) {
-
+    vector < int > nextGreaterElement(vector < int > & nums1, vector < int > & nums2) 
+    {
       unordered_map < int, int > mp;
-
       stack < int > st;
-
+        //finding N.G.E for entire nums2 vector and atlast selecting only nums1 element
       for (int i = nums2.size() - 1; i >= 0; i--) 
       {
         if (st.size() == 0) 
-        {
-          mp[nums2[i]] = -1;
-        } 
-          
-        else if (st.top() > nums2[i]) 
-        {
-          mp[nums2[i]] = st.top();
-
-        } 
-          else 
-          {
-
-          while (st.size() > 0 && st.top() <= nums2[i]) 
-          {
-            st.pop();
-
-          }
-
-          if (st.size() == 0) {
             mp[nums2[i]] = -1;
-
-          } else {
+        else if (st.top() > nums2[i]) 
             mp[nums2[i]] = st.top();
-
-          }
-
+         
+        else 
+        {
+          while (st.size() > 0 && st.top() <= nums2[i]) 
+              st.pop();
+          if (st.size() == 0) 
+              mp[nums2[i]] = -1;  
+          else 
+              mp[nums2[i]] = st.top();
         }
+          
         st.push(nums2[i]);
       }
+        //pushing only nums1 entry into answer vector
       vector < int > ans;
       for (auto elem: nums1) 
       {
         ans.push_back(mp[elem]);
       }
       return ans;
-
     }
 };
