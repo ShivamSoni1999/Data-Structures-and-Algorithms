0910288cef30d7147c3d4f5942e2ec0e99981f37
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-06-15 00:43:26 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
  // } Driver Code Ends
 class Solution
-{
+{ 
+    //Top down memoization approach
     public:
-    //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
         vector<vector<int>> t(n+1, vector<int>(W+1, -1));
@@ -16,20 +16,19 @@ class Solution
     int fun(int w, int wt[], int val[], int n, vector<vector<int>>& t) 
     { 
       //Base Case
-      if(n<=0 || w<=0) return 0;
+      if(n==0 || w==0) return 0;
        
       //Check if result exists already
       if(t[n][w] != -1) return t[n][w];
        
         //If result doesn't exist
-        //Choice Diagram Code 
         //1. Choice to include item or not
         if(wt[n-1] <= w){
             t[n][w] = max(val[n-1] + fun(w-wt[n-1], wt, val, n-1, t), 0+fun(w,wt,val,n-1, t));
             return t[n][w];
         }
         
-        //2. No choice
+        //2. leaving Item
         else if(wt[n-1] > w){
             t[n][w] = 0 + fun(w,wt,val,n-1, t);
             return t[n][w];
@@ -37,9 +36,6 @@ class Solution
         
     
     }
-  
-        
-    
 };
 
 // { Driver Code Starts.
