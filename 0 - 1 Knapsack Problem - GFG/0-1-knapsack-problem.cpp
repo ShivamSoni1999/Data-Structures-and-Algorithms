c75434272ce6b41bae33c72c83d7cde6fb18aebc
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-06-15 00:37:20 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -10,24 +10,32 @@ class Solution
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-       // Your code here
-     int t[n + 1][W + 1]; // DP matrix
-
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= W; j++) {
-			if (i == 0 || j == 0) // base case // filling 1st row and 1st column of the matrix with zero as per the base condition of the recursive solution 
-				t[i][j] = 0;
-			else if (wt[i - 1] <= j) { // current wt can fit in bag // this is for the choice diagram of the recursive solution
-				int val1 = val[i - 1] + t[i - 1][j - wt[i - 1]]; // take current wt // and after taking weight substract the inserted weight from the final weight 
-				int val2 = t[i - 1][j]; // skip current wt
-				t[i][j] = max(val1, val2);
-			}
-			else if (wt[i - 1] > j) // current wt doesn't fit in bag
-				t[i][j] = t[i - 1][j]; // move to next
-		}
-	}
-
-	return t[n][W];
+        vector<vector<int>> t(n+1, vector<int>(W+1, -1));
+        return fun(W, wt, val, n, t);
+    }
+    int fun(int w, int wt[], int val[], int n, vector<vector<int>>& t) 
+    { 
+      //Base Case
+      if(n<=0 || w<=0) return 0;
+       
+      //Check if result exists already
+      if(t[n][w] != -1) return t[n][w];
+       
+        //If result doesn't exist
+        //Choice Diagram Code 
+        //1. Choice to include item or not
+        if(wt[n-1] <= w){
+            t[n][w] = max(val[n-1] + fun(w-wt[n-1], wt, val, n-1, t), 0+fun(w,wt,val,n-1, t));
+            return t[n][w];
+        }
+        
+        //2. No choice
+        else if(wt[n-1] > w){
+            t[n][w] = 0 + fun(w,wt,val,n-1, t);
+            return t[n][w];
+        }
+        
+    
     }
   
         
