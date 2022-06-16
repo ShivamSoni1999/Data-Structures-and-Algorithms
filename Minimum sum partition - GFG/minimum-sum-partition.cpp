0bf461eb20ebcd6e65bbb1b55ff4e34445e3be2a
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-06-16 17:22:25 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -6,46 +6,44 @@ using namespace std;
 class Solution{
 
   public:
-    vector<int> subsetsum(int arr[],int n,int sum){
-        bool t[n + 1][sum + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= sum; j++) {
-                if (i == 0)
-                  t[i][j] = false;
-                if (j == 0)
-                  t[i][j] = true;
-            }
+    vector < int > subsetsum(int arr[], int n, int sum) {
+      bool t[n + 1][sum + 1];
+      for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= sum; j++) {
+          if (i == 0)
+            t[i][j] = false;
+          if (j == 0)
+            t[i][j] = true;
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                if (arr[i - 1] <= j)
-                  t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j]; // include or exclude
-                else
-                  t[i][j] = t[i - 1][j]; // exclude
-            }
+      }
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+          if (arr[i - 1] <= j)
+            t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j]; // include or exclude
+          else
+            t[i][j] = t[i - 1][j]; // exclude
         }
-        vector<int> v;
-        for (int j = 0; j <= sum; j++)
-        {
-            if(t[n][j]==true)
-                v.push_back(j);
-        }
-        return v;
+      }
+      vector < int > v;                 // contains all subset sums possible with n elements of last row
+      for (int j = 0; j <= sum; j++) {  //from the range(sum) we need to exclude the element which is not there in the existing problem
+        if (t[n][j] == true)            //keep true to only those place whose subset sum exist
+          v.push_back(j);
+      }
+      return v;
     }
-	int minDifference(int arr[], int n)  { 
-	    int mn=INT_MAX;
-	    int sum=0; //range
-	    for(int i=0;i<n;i++)
-	        sum=sum+arr[i];
-	        
-	    vector<int> v = subsetsum(arr,n,sum);
-	    for (int i = 0; i < v.size(); i++)
-	    {
-	        mn = min(mn, abs(sum - 2 * v[i]));
-	    }
-	    return mn;
-	    
-	} 
+  int minDifference(int arr[], int n) {
+    int mn = INT_MAX;
+    int sum = 0; //range
+    for (int i = 0; i < n; i++)
+      sum = sum + arr[i];
+
+    vector < int > v = subsetsum(arr, n, sum);
+    for (int i = 0; i < v.size(); i++) {
+      mn = min(mn, abs(sum - 2 * v[i]));
+    }
+    return mn;
+
+  }
 };
 
 
