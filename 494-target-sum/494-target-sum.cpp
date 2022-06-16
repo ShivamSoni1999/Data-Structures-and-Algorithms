f574a46df8062ff89f8e306c1c75da6488e0e996
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-06-17 00:03:09 +0530

    Time: 29 ms (53.98%), Space: 12.2 MB (41.33%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,34 +1,33 @@
 class Solution {
-public:
+  int countsubset(vector < int > & nums, int sum) {
+    int n = nums.size();
+    vector < vector < int >> t(n + 1, vector < int > (sum + 1, 0));
 
-       int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-		// variable to store sum of array nums
-        int sm=0;
-        for(auto x : nums)  sm+=x;
-		// base cases , sum should be greater then target and difference should always be even for equal subsets
-        if(sm<target || (sm-target)%2!=0)   return 0;
-        // finding the value of other subset
-        int sm2 = (sm-target)/2;
-		// 2D vector to store the sub-problem solutions
-        vector<vector<int>> t(n+1,vector<int>(sm2+1,0));
-		// 0 elements required to get 0 sum
-        t[0][0]=1;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=0;j<=sm2;j++)
-            {
-				// if value is less than j then we have chance to include it 
-                if(nums[i-1]<=j)
-                    t[i][j] = t[i-1][j] + t[i-1][j-nums[i-1]];
-				// we use the earlier same solution for it
-                else
-                    t[i][j] = t[i-1][j];
-            }
-        }
-		// to have target we have t[n][sm2] ways
-        return t[n][sm2];
-        
-    
+    t[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j <= sum; j++) {
+
+        if (nums[i - 1] <= j)
+          t[i][j] = t[i - 1][j] + t[i - 1][j - nums[i - 1]];
+
+        else
+          t[i][j] = t[i - 1][j];
+      }
+    }
+    return t[n][sum];
+  }
+  public:
+
+    int findTargetSumWays(vector < int > & nums, int target) {
+      int n = nums.size();
+
+      int sum = 0;
+      for (auto x: nums) sum += x;
+      if (sum < target || (sum - target) % 2 != 0) return 0;
+
+      int sm2 = (sum - target) / 2;
+
+      return countsubset(nums, sm2);
+
     }
 };
