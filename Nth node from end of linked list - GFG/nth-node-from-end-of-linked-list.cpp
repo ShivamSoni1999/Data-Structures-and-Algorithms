9d6f02b38488afdd562d79e202b1ae9c275e1980
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-06-28 03:48:34 +0530

    Added solution - LeetHub

---
+++ Nth node from end of linked list - GFG/nth-node-from-end-of-linked-list.cpp
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+    cout<<getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(Node *head, int n)
+{
+       // Your code here
+      Node *ptr1 = head;
+      Node *ptr2 = head;
+      for(int i=1; i<n; i++) //move first pointer to nth node and let the 2nd pointer be at head
+         {
+             ptr1=ptr1->next;
+             if(!ptr1)
+             return -1;
+         }
+         
+         while(ptr1->next != NULL) //move both pointer untill the 1st pointer reach the last pointer
+         {
+             ptr1= ptr1->next;
+             ptr2=ptr2->next;
+         }
+         return ptr2->data; //at this point 2nd pointer will be at nth node from the last.
+
+}
+
