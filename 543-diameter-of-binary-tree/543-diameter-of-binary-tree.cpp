95a605dd410e0e1dbeb08942b1cbcf5b0d8b64d9
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-07-02 16:56:30 +0530

    Time: 14 ms (67.34%), Space: 20.3 MB (26.01%) - LeetHub

---
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int res;
+public:
+    int Solve(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+
+        int l = Solve(root->left);
+        int r = Solve(root->right);
+
+        int temp = 1 + max(l, r);        //if currunt node doesn't want to be part of the answer
+        int ans = max(temp, l + r + 1);  //if cur node wants to be answer
+        res = max(res, ans);
+        return temp;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+
+        res = INT_MIN + 1;
+        int x=Solve(root);
+        return res - 1;
+    }
+};
