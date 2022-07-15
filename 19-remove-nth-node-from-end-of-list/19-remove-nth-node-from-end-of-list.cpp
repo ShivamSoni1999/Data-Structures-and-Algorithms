5d537dda4f61ad220e1af70577b99989cfb1de07
Shivam Nandha <37973948+ShivamSoni1999@users.noreply.github.com>
2022-07-15 23:59:32 +0530

    Time: 12 ms (16.69%), Space: 10.6 MB (96.38%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -3,7 +3,9 @@ class Solution {
 public:
    ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode *fast = head, *slow = head;
-        while(n--) fast = fast -> next;      // iterate first n nodes using fast
+        while(n--) 
+            fast = fast -> next;      // iterate first n nodes using fast
+       
         if(fast==NULL) return head -> next; // if fast is already null, it means we have to delete head itself. So,just return next of head
        
         while(fast -> next)                  // iterate till fast reaches the last node of list
